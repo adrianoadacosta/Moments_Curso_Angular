86947ebabe529c60eab9af32a54180e8f3945a65
adrianoadacosta <adrianoadacosta@outlook.com>
2023-01-24 21:48:35 -0300

    Finalizando pagina Home

--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -3,16 +3,20 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Moment } from '../Moment';
 import { environment } from 'src/environments/environment';
-
+import { Response } from '../Response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MomentService {
-  private baseApiUrl = environment.baseApiUrl
-  private apiUrl = `${this.baseApiUrl}api/moments`
+  private baseApiUrl = environment.baseApiUrl;
+  private apiUrl = `${this.baseApiUrl}api/moments`;
 
   constructor(private http: HttpClient) { }
+  
+  getMoments(): Observable<Response<Moment []>> {
+    return this.http.get<Response<Moment []>>(this.apiUrl);
+  }
 
   createMoment(formData: FormData): Observable<FormData>{
     return this.http.post<FormData>(this.apiUrl, FormData);

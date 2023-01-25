9d00cd0dbe65a1cb45318e03d9a1cbf2a5322445
adrianoadacosta <adrianoadacosta@outlook.com>
2023-01-25 01:23:21 -0300

    Finalizando pagina detalhes

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -14,6 +14,9 @@ export class HomeComponent implements OnInit{
   moments: Moment [] = [];
   baseApiurl = environment.baseApiUrl;
 
+  faSearch = faSearch;
+  searchTerm: string = ''; 
+
   constructor(private momentService: MomentService){}
 
   ngOnInit(): void {
@@ -27,5 +30,13 @@ export class HomeComponent implements OnInit{
       this.allMoments = data;
       this.moments = data;
     })
+  }
+  search(e: Event): void {
+    const target = e.target as HTMLInputElement;
+    const value = target.value;
+
+    this.moments = this.allMoments.filter((moment) => {
+      return moment.title?.toLowerCase().includes(value);
+    });
   }
 }
